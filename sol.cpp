e5b89cd31a7f8e61f0f70bc93fe9f714c58c1cb7
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-09-05 18:36:21 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -4,11 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int T;
-	scanf("%d", &T);
+	cin >> T;
 	while(T--) {
 		int l, r;
-		scanf("%d%d", &l, &r);
-		printf("%d %d\n", l, l + l);
+		cin >> l >> r;
+		cout << l << " " << l + l << "\n";
 	}
 }
